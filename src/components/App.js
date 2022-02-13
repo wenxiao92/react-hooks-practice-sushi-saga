bab12632091f24098105a8c5c236833673c7759c
Wen Xiao <wennstyle@gmail.com>
2022-02-12 22:42:34 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [currentList, setSushi] = useState([])
+  const [money, setMoney] = useState(100)
+
+  useEffect(() => {
+    fetch(`${API}`)
+    .then((r) => r.json())
+    .then((allSushi) => {
+      const updatedSushis = allSushi.map((sushi) => {
+        return {...sushi, eaten: false}; //adds to each object a key of eaten with a boolean value of false
+      });
+      setSushi(updatedSushis)
+    })
+  },[])
+
+  function handleEatSushi(eatenSushi){
+    //if statement to wrap the eatenSushi to figure out if we can eat it
+    if(money >= eatenSushi.price){
+    //handles the changing of the status of the sushi. This gets wrapped within an if statement
+    const changeStatus = currentList.map((sushi)=>{
+      if(sushi.id === eatenSushi.id) return {...sushi, eaten: true};
+      return sushi;
+    })
+    setSushi(changeStatus)
+    setMoney((money) => money-eatenSushi.price) //if the sushi can be afforded, change state of wallet
+  } else {
+    alert("Need more $$")
+  }
+  }
+
+  //created to be passed as a prop to Table for the eaten status
+  const eatenSushis = currentList.filter((sushi) => sushi.eaten)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushiList={currentList} onEatSushi={handleEatSushi}/>
+      <Table plates={eatenSushis} wallet={money}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({sushiList, onEatSushi}) {
+
+  //console.log(sushiList)
+  const [sushiIndex, setSushiIndex] = useState(0)
+
+  const sushiComponents = sushiList
+  //grabs the first four sushi from sushiList by grabbing initially grabbing 0,4
+  .slice(sushiIndex, sushiIndex + 4)
+  //sends the four sushi from sushiList to Sushi component using map. Map method must come second
+  .map((sushi) => (                   
+    <Sushi
+      key={sushi.id}
+      sushi={sushi}
+      onEatSushi={onEatSushi}
+      />
+  ))
+
+  function handleNextSet() {
+    //adds 4 to index. So next set is spliced from 4,8. Returning the 4th, 5th, 6th and 7th sushi
+    setSushiIndex((sushiIndex) => (sushiIndex + 4))
+
+  }
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      {/* click to handle adding more sushi */}
+      <MoreButton onNextSushi={handleNextSet}/>
     </div>
   );
 }
